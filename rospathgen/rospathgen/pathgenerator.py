7930f2b73c0c89de1048fd15510c1c3e2f7525de
Tyler Zupfer <tzupfer@gmail.com>
2021-08-25 23:25:37 -0400

    Finishing things up

--- rospathgen/rospathgen/pathgenerator.py
+++ rospathgen/rospathgen/pathgenerator.py
@@ -1,11 +1,13 @@
+from types import new_class
 from typing import Optional
 import rclpy
 from rclpy import Node
 from rospathmsgs.srv import GeneratePath
 import numpy as np
 from scipy.interpolate import CubicSpline
-from geometry_msgs.msgs import Vector3
+from geometry_msgs.msg import Vector3
 from rospathmsgs.msg import Waypoint
+import math
 
 class Point():
     name = ""
@@ -21,39 +23,74 @@ class Point():
 class Waypoint():
     speed = 0
     name = ""
-    point = Point(0,0)
-    def __init__(self, point: Point, speed, name):
-        self.point = point
+    x = 0
+    y = 0
+    z = 0
+    def __init__(self, xval, yval, zval = None, speed = 0, name = ""):
+        self.x = xval
+        self.y = yval
+        self.z = zval
         self.speed = speed
         self.name = name
 
 class pathGen(Node):
-    pointInput = [Point]
+    pointsInput = [Point]
     pointsOutput = [Point]
     waypointsOutput = [Waypoint]
+    maxAccelConst = 8 
+    maxVelConst = 10 
     
     def generatepathcallback(self, request, response):
         for i in range(request.points):
             x = request.points[i].point.x
             y = request.points[i].point.y
-            newPoint = Point(x, y)
-            self.pointInput.append(newPoint)
-        maxAccelConst = request.maxAccel
-        maxVelConst = request.maxVel
+            if not request.points[i].point.name:
+                name = request.points[i].point.name
+                newPoint = Point(x, y, name= name)
+            else:
+                newPoint = Point(x, y)
+            self.pointsInput.append(newPoint)
+        self.maxAccelConst = request.maxAccel
+        self.maxVelConst = request.maxVel
         
         # points into lists of x and y inputs
-        for point in self.pointInput:
+        for point in self.pointsInput:
             self.xpoints.append(point.x)
             self.ypoints.append(point.y)
 
-        # make a spline
-        cs = CubicSpline(self.xpoints, self.ypoints)
+        # make a spline for the path
+        path = CubicSpline(self.xpoints, self.ypoints)
 
         for xval in np.arange(self.xpoints[0], self.xpoints[len(self.xpoints) - 1], 0.01):
-            # TODO do the math things and figure out how to store things in the way I want
-            # Use the waypoint msg
-            filler
+            yval = path(xval)
+            newPoint = Point(xval, yval, name= self.pointsInput[self.xpoints.index(xval)].name)
+            self.pointsOutput.append(newPoint)
+            
+        self.distgraphxs = []
+        self.distgraphys = []
+        
+        self.distgraphxs.append(0)
+        self.distgraphys.append(0)
 
+        for point in self.pointsOutput:
+            # make the deriv graph loop
+            # speed is the deriv of the dist vs time graph
+
+            # Calculate Distances
+            distance = math.sqrt(pow((self.distgraphxs[len(self.distgraphxs)-1]-point.x), 2) 
+                + pow((self.distgraphys[len(self.distgraphys)-1]-point.y), 2))
+            self.distgraphxs = self.pointsOutput.index(point) / 100
+            self.distgraphys = distance
+
+        distancegraph = CubicSpline(self.distgraphxs, self.distgraphys)
+        derivdistgraph = distancegraph.derivative()
+        
+        for point in self.pointsOutput:
+            newWaypoint = Waypoint(point.x, point.y, speed= derivdistgraph(self.pointsOutput.index(point) / 100), name= point.name)
+            self.waypointsOutput.append(newWaypoint)
+
+
+        
     def __init__(self):
         super().__init__('pathGen')
         self.pathService = self.create_service(GeneratePath, 'generate_path', self.generatepathcallback)
