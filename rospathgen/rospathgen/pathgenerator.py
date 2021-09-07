f96284e57efa0d1b1f0e6ee517b8c8577eb30b6b
Tyler Zupfer <tzupfer@gmail.com>
2021-09-07 19:06:36 -0400

    finishing

--- rospathgen/rospathgen/pathgenerator.py
+++ rospathgen/rospathgen/pathgenerator.py
@@ -1,28 +1,12 @@
-from types import new_class
-from typing import Optional
 import rclpy
 from rclpy import Node
 from rospathmsgs.srv import GeneratePath
 import numpy as np
 from scipy.interpolate import CubicSpline
-from geometry_msgs.msg import Vector3
 from rospathmsgs.msg import Waypoint
-import math
-
-class Waypoint():
-    speed = 10
-    name = ""
-    x = 0
-    y = 0
-    def __init__(self, xval, yval, speed = 10, name = ""):
-        self.x = xval
-        self.y = yval
-        self.speed = speed
-        self.name = name
+from geometry_msgs.msg import Vector3
 
 class pathGen(Node):
-    pointsInput = [Waypoint]
-    pointsOutput = [Waypoint]
     waypointsOutput = [Waypoint]
     maxAccelConst = 8 
     maxVelConst = 10 
@@ -31,17 +15,10 @@ class pathGen(Node):
         for i in range(request.points):
             x = request.points[i].point.x
             y = request.points[i].point.y
-            if not request.points[i].point.name:
-                name = request.points[i].point.name
-                newPoint = Waypoint(x, y, speed =  request.points[i].point.speed, name= name)
-            else:
-                newPoint = Waypoint(x, y, speed =  request.points[i].point.speed)
-            self.pointsInput.append(newPoint)
-        self.maxAccelConst = request.maxAccel
-        
-        # points into lists of x and y inputs
-        self.xpoints.append(x)
-        self.ypoints.append(y)            
+            # points into lists of x and y inputs
+            self.xpoints.append(x)
+            self.ypoints.append(y)   
+        self.maxAccelConst = request.maxAccel         
 
         # make a spline for the path
         path = CubicSpline(self.xpoints, self.ypoints)
@@ -50,12 +27,14 @@ class pathGen(Node):
         for xval in np.arange(self.xpoints[0], self.xpoints[len(self.xpoints) - 1], 0.01):
             yval = path(xval)
             if (self.xpoints.index(xval) != -1):
-                newPoint = Waypoint(xval, yval, speed= self.pointsInput[self.xpoints.index(xval)].speed, name= self.pointsInput[self.xpoints.index(xval)].name)
+                newPoint = Waypoint(Vector3(xval, yval, 0),  speed= self.pointsInput[self.xpoints.index(xval)].speed, name= self.pointsInput[self.xpoints.index(xval)].name)
                 currentMaxSpeed = self.pointsInput[self.xpoints.index(xval)].speed
             else:
-                newPoint = Waypoint(xval, yval, speed = currentMaxSpeed)
+                newPoint = Waypoint(Vector3(xval, yval, 0), speed = currentMaxSpeed)
             self.pointsOutput.append(newPoint)
+        response.waypoints = self.pointsOutput
 
+        return response
 
         
     def __init__(self):
