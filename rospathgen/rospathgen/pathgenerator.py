9fad289859e4807adb78e95d0cf36d3207e66338
Tazmania40 <tzupfer@gmail.com>
2022-01-11 20:16:48 -0500

    fix path gengit

--- rospathgen/rospathgen/pathgenerator.py
+++ rospathgen/rospathgen/pathgenerator.py
@@ -6,47 +6,58 @@ from scipy.interpolate import CubicSpline
 from rospathmsgs.msg import Waypoint
 from geometry_msgs.msg import Vector3
 
-class pathGen(Node):
-    pointsOutput = [Waypoint]
-    maxAccel = 8 
-    maxVelConst = 10 
-    xpoints = []
-    ypoints = []
+class pathGen(Node):  
     
     def generatepathcallback(self, request, response):
+        pointsInput = []
+        pointsOutput = []
+        maxAccel = 8 
+        maxVelConst = 10 
+        xpoints = []
+        ypoints = []
         """Give a list of waypoints, gives back entire path"""
         try: 
             print(request)
-            for point in len(request.points):
+            print(request.points)
+            pointsInput = request.points
+            for point in range(len(request.points)):
                 x = request.points[point].point.x
                 y = request.points[point].point.y
                 # points into lists of x and y inputs
-                self.xpoints.append(x)
-                self.ypoints.append(y)   
-            self.maxAccel = request.max_accel         
+                xpoints.append(x)
+                ypoints.append(y)
+            maxAccel = request.max_accel         
 
             # make a spline for the path
-            path = CubicSpline(self.xpoints, self.ypoints)
+            path = CubicSpline(xpoints, ypoints)
 
             currentMaxSpeed = 0
-            for xval in np.arange(self.xpoints[0], self.xpoints[len(self.xpoints) - 1], 0.01):
-                yval = path(xval)
+            print(xpoints)
+            for xval in np.arange(xpoints[0], xpoints[len(xpoints) - 1], 0.01):
+                yval = float(path(xval))
                 # If the xpoint is on the list, make it a point, immigrate data.
-                if (self.xpoints.index(xval) != -1):
-                    pointname = self.pointsInput[self.xpoints.index(xval)].name
+                if (xpoints.count(xval) != 0):
+                    pointname = pointsInput[xpoints.index(xval)].point_name
                     # Create a point, if no name input, name = empty string so no errors
-                    newPoint = (Waypoint(Vector3(xval, yval, 0), speed = self.pointsInput[self.xpoints.index(xval)].max_vel, 
-                            name = pointname if pointname is not None else ""))
-                    currentMaxSpeed = self.pointsInput[self.xpoints.index(xval)].max_vel
+                    newPoint = Waypoint(point=Vector3(x=xval, y=yval, z=0.0), 
+                                        heading= pointsInput[xpoints.index(xval)].heading, 
+                                        max_vel= pointsInput[xpoints.index(xval)].max_vel, 
+                                        point_name= pointname if pointname is not None else "")
+                    currentMaxSpeed = pointsInput[xpoints.index(xval)].max_vel
                 else:
                     # If not on list, make a point
-                    newPoint = Waypoint(Vector3(xval, yval, 0), speed = currentMaxSpeed)
-                self.pointsOutput.append(newPoint)
+                    newPoint = Waypoint(point=Vector3(x=xval, y=yval, z=0.0), 
+                                        heading= 0.0, # TODO Add heading enforcement
+                                        max_vel= currentMaxSpeed, 
+                                        point_name= "")
+                pointsOutput.append(newPoint)
             # Connect created list of points to output
-            response.waypoints = self.pointsOutput
+            response.waypoints = pointsOutput
 
             return response
-        except: 
+        except Exception as e:
+            print(e) 
+            print("i did a dumb")
             return response
 
         
--- rospathgen/rospathgen/pathtester.py
+++ rospathgen/rospathgen/pathtester.py
@@ -2,34 +2,72 @@ import rclpy
 from rclpy.node import Node
 from rospathmsgs.srv import GeneratePath
 import numpy as np
-from scipy.interpolate import CubicSpline
 from rospathmsgs.msg import Waypoint
 from geometry_msgs.msg import Vector3
+import matplotlib.pyplot as plt
 
-class pathTester(Node):
-    pointsOutput = [Waypoint]
-    maxAccel = 8 
-    maxVelConst = 10 
-    xpoints = []
-    ypoints = []
+class tester(Node):
 
-    
     def __init__(self):
-        super().__init__('minimal_service')
-        self.srv = self.create_service(pathGen, 'add_two_ints', self.add_two_ints_callback)
+        super().__init__('path_tester')
+        self.client = self.create_client(GeneratePath, 'generate_path')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.request = GeneratePath.Request()
 
-    def add_two_ints_callback(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+    def send_request(self):
+        # Add point to list in format 
+        # Waypoint(Vector3(x, y, z), heading, max_vel, point_name)
+        self.request.points = [ Waypoint(point=Vector3(x=0.0, y=0.0, z=0.0), 
+                                    heading=0.0, 
+                                    max_vel=10.0, 
+                                    point_name="Start"),
+                                Waypoint(point=Vector3(x=5.0, y=0.0, z=0.0), 
+                                    heading=0.0, 
+                                    max_vel=10.0, 
+                                    point_name="End")]
+        self.request.max_accel = 20.0
+        self.request.max_angular_vel = 20.0
+        self.future = self.client.call_async(self.request)
 
-        return response
+def graphOutput(response):
+    xvals = []
+    yvals = []
+    
+    pointList = response.waypoints
+    for point in pointList:
+        xvals.append(point.point.x)
+        yvals.append(point.point.y)
+        if point.point_name != "":
+            name = point.point_name
+            name = str(name)
+            #plt.text(xvals, yvals, name)        
 
-# Run a node, don't try to understand
-def main(args=None):
-    rclpy.init(args=args)
-    pathgen = pathGen()
-    rclpy.spin(pathgen)
-    pathgen.destroy_node()
+    #print(xvals, yvals)
+    plt.plot(xvals,yvals, 'bo')
+    plt.xlabel('X value (m)')
+    plt.ylabel('Y value (m)')
+    plt.show()
+
+def main():
+    rclpy.init()
+
+    pathTester = tester()
+    pathTester.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(pathTester)
+        if pathTester.future.done():
+            try:
+                response = pathTester.future.result()
+            except Exception as e:
+                pathTester.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                graphOutput(response)
+            break
+
+    pathTester.destroy_node()
     rclpy.shutdown()
 
 
